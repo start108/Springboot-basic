bf8924bb446e4add70ca7c5616068f6a591e0339
cjy <start108@naver.com>
2022-05-08 11:00:49 +0900

    JdbcTemplate 활용, 테스트 코드 작

--- src/main/java/com/springbootbasic/SpringConfig.java
+++ src/main/java/com/springbootbasic/SpringConfig.java
@@ -1,14 +1,25 @@
 package com.springbootbasic;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.springbootbasic.repository.MemoryMemberRepository;
+import com.springbootbasic.repository.JdbcTemplateMemberRepository;
+import com.springbootbasic.repository.MemberRepository;
 import com.springbootbasic.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 
+	private DataSource dataSource;
+	
+	@Autowired
+	public SpringConfig( DataSource dataSource ) {
+		this.dataSource = dataSource;
+	}
+	
 	/*
 	 * 직접 스프링 빈 등록
 	 * */
@@ -18,7 +29,8 @@ public class SpringConfig {
 	}
 	
 	@Bean
-	public MemoryMemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+	public MemberRepository memberRepository() {
+		//return new MemoryMemberRepository();
+		return new JdbcTemplateMemberRepository( dataSource );
 	}
 }
